f6651d05e437a56cf9edd59a653c105b15513b79
VitorGDellino <vitor.dellinocente@usp.br>
2018-05-23 19:54:27 -0300

    Alguns codigos js

---
+++ T2/src/js/changetab.js
@@ -0,0 +1,18 @@
+function goToHome(){
+    $(document).ready( function(){
+        $("#mutableContent").load("../html/home.html");
+    });
+
+}
+
+function goToProducts(){
+    $(document).ready( function(){
+        $("#mutableContent").load("../html/produtos.html");
+    });
+}
+
+function goToServices(){
+    $(document).ready( function(){
+        $("#mutableContent").load("../html/servicos.html");
+    });
+}
---
+++ T2/src/js/login.js
@@ -0,0 +1,5 @@
+async function login(){
+    var username = await document.getElementById("username").value.toString();
+    var password = await document.getElementById("password").value.toString();
+
+}
